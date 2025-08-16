d255270fdc46e0d3b3cd9e77650f457a24688e11
taiyebaafrin <taiyebasadia753@gmail.com>
2025-08-16 18:01:52 +0600

    mid term

--- tasks/models.py
+++ tasks/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# class Participant(models.Model):
+#     name = models.CharField(max_length=100)
+#     email = models.EmailField(unique=True)
 
+#     def __str__(self):
+#         return self.name
+    
 class Event(models.Model):
     STATUS_CHOICES = [
         ('PENDING', 'Pending'),
